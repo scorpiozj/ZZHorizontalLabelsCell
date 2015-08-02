223e678095c4f59297e4e5b749f13e36482cc41b
ZhuJiang <zhujiang@promote.cache-dns.local>
2015-08-02 10:02:14 +0800

    Initial Commit

---
+++ CellLabelsLayoutCollection/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CellLabelsLayoutCollection
+//
+//  Created by ZhuJiang on 15/8/2.
+//  Copyright (c) 2015年 Charles Zhu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ CellLabelsLayoutCollection/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CellLabelsLayoutCollection
+//
+//  Created by ZhuJiang on 15/8/2.
+//  Copyright (c) 2015年 Charles Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
