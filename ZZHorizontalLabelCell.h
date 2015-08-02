06baae829248bfd361e175327ce8f0c8897595c4
ZhuJiang <zhujiang@promote.cache-dns.local>
2015-08-02 10:07:39 +0800

    Add 1 add TableView to mainviewcontroller 2 init add: ZZHorizontalLabelCell

---
+++ ZZHorizontalLabelCell.h
@@ -0,0 +1,13 @@
+//
+//  ZZHorizontalLabelCell.h
+//  CellLabelsLayoutCollection
+//
+//  Created by ZhuJiang on 15/8/2.
+//  Copyright (c) 2015年 Charles Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZZHorizontalLabelCell : UITableViewCell
+
+@end
---
+++ ZZHorizontalLabelCell.m
@@ -0,0 +1,23 @@
+//
+//  ZZHorizontalLabelCell.m
+//  CellLabelsLayoutCollection
+//
+//  Created by ZhuJiang on 15/8/2.
+//  Copyright (c) 2015年 Charles Zhu. All rights reserved.
+//
+
+#import "ZZHorizontalLabelCell.h"
+
+@implementation ZZHorizontalLabelCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
